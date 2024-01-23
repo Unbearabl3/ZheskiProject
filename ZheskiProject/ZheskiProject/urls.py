26f2943725d573b64aa8bbe25ea6a1ec7b3b77e1
hiostage <v.gaberkorn@mail.ru>
2024-01-24 03:38:13 +0600

    main

--- ZheskiProject/ZheskiProject/urls.py
+++ ZheskiProject/ZheskiProject/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from users.views import reg
+from main.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('reg/', reg, name='reg'),
-]
+    path('', index, name='index'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
