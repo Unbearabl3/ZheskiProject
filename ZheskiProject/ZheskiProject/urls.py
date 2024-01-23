3eb141208ad50b272518db06ce4e9a2a46d6a9e3
hiostage <v.gaberkorn@mail.ru>
2024-01-24 01:17:01 +0600

    users

--- ZheskiProject/ZheskiProject/urls.py
+++ ZheskiProject/ZheskiProject/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from users.views import reg
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('reg/', reg, name='reg'),
 ]
