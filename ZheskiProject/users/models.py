f593061bb64b5f5b7808ad682f9ad7d22693c629
hiostage <v.gaberkorn@mail.ru>
2024-03-08 13:33:04 +0500

    bd

--- ZheskiProject/users/models.py
+++ ZheskiProject/users/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 
-class Registration(models.Model):
+class Users(models.Model):
     UserName = models.CharField('Username', max_length=32)
     Password = models.CharField('Password', max_length=32)
     Email = models.EmailField('Email')
-class Authorization(models.Model):
-    UserName = models.CharField('Username', max_length=32)
-    Password = models.CharField('Password', max_length=32)
-# Create your models here.
+
+    def __str__(self):
+        return self.UserName
+    
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
